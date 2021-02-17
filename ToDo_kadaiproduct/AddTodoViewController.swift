206ce90cdeb9f1c3e9a53721c15fd3b94b5a5bd9
ayu129 <ayu.daddybear.29@gmail.com>
2021-02-17 18:26:54 +0900

    追加はできた

--- ToDo_kadaiproduct/AddTodoViewController.swift
+++ ToDo_kadaiproduct/AddTodoViewController.swift
@@ -12,10 +12,10 @@ class AddTodoViewController: UIViewController {
     @IBOutlet var datePicker: UIDatePicker!
     @IBOutlet var todoTextField: UITextField!
     
-    var todoList = [String]()
     var saveData: UserDefaults = UserDefaults.standard
     
     var formatter = DateFormatter()
+    var dateString: String!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,13 +38,29 @@ class AddTodoViewController: UIViewController {
     */
     
     //UIDatePickerの値が変わった時に呼び出されるメソッド
-    @IBAction func changeDate(_ sender: UIDatePicker) {
-       formatter.dateFormat = "yyyy/MM/dd HH:mm:ss"
-    }
+    //@IBAction func changeDate(_ sender: UIDatePicker) {
+       //formatter.dateFormat = "yyyy/MM/dd"
+    //}
     
     
-    @IBAction func save(){
-        
+//    @IBAction func save(){
+//        formatter.dateFormat = "yyyy/MM/dd"
+//        dateString = formatter.string(from: datePicker.date)
+//        todoList_date.append(dateString)
+//        saveData.set(todoList_date, forKey: "todo_Date")
+//        todoList_content.append(todoTextField.text ?? "")
+//        saveData.set(todoList_content, forKey: "todo_Content")
+//        print("number3")
+//    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "backToFirst" {
+            formatter.dateFormat = "yyyy/MM/dd"
+            dateString = formatter.string(from: datePicker.date)
+            let firstViewController = segue.destination as! ViewController
+            firstViewController.todoDate = self.dateString
+            firstViewController.todoContent = self.todoTextField.text
+        }
     }
     
     @IBAction func cancel(){
--- ToDo_kadaiproduct/ViewController.swift
+++ ToDo_kadaiproduct/ViewController.swift
@@ -7,12 +7,57 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todoList_Date.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        cell?.detailTextLabel?.text = todoList_Date[indexPath.row]
+        cell?.textLabel?.text = todoList_Content[indexPath.row]
+        cell?.detailTextLabel?.textColor = UIColor(red: 105/255, green: 105/255, blue: 105/255, alpha: 1)
+        return cell!
+    }
+    
 
+    @IBOutlet var table: UITableView!
+    
+    var saveData: UserDefaults = UserDefaults.standard
+    var todoList_Date = [String]()
+    var todoList_Content = [String]()
+    var todoDate: String!
+    var todoContent: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        table.dataSource = self
+        
+        todoList_Date = saveData.array(forKey: "todo_Date") as? [String] ?? []
+        todoList_Content = saveData.array(forKey: "todo_Content") as? [String] ?? []
+        
+        
     }
+    
+    @IBAction func myUnwindAction(for unwindSegue: UIStoryboardSegue, towardsViewController subsequentVC: UIViewController) {
+        if unwindSegue.identifier == "backToFirst" {
+            todoList_Date = saveData.array(forKey: "todo_Date") as? [String] ?? []
+            todoList_Content = saveData.array(forKey: "todo_Content") as? [String] ?? []
+            todoList_Date.append(todoDate)
+            todoList_Content.append(todoContent ?? "")
+            saveData.set(todoList_Date, forKey: "todo_Date")
+            saveData.set(todoList_Content, forKey: "todo_Content")
+            table.reloadData()
+        }
+    }
+    
+    
+//    @IBAction func backToFirst(_ segue: UIStoryboardSegue) {
+//
+//    }
+
 
 
 }
